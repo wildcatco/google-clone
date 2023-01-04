5eb58ee5583fe3e9c1019b964bbb9007883d66cd
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-04 16:39:40 +0900

    Create the header section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/header";
 
 export default function Home() {
   return (
@@ -10,7 +11,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="font-bold">Hello world!</h1>
+      {/* Header */}
+      <Header />
+
+      {/* Body */}
+
+      {/* Footer */}
     </>
   );
 }
