6da356a51dfa0a249d968c6a4f5a7bd30c91b1e9
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-05 19:26:04 +0900

    Add links on About, Store, Gamil, Images buttons in header

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,38 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import User from "./user";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <header className="flex justify-between p-5 text-sm text-gray-700">
       <div className="flex space-x-4 items-center">
-        <p className="link">About</p>
-        <p className="link">Store</p>
+        <Link href="https://about.google/">
+          <p className="link">About</p>
+        </Link>
+        <Link href="https://store.google.com/">
+          <p className="link">Store</p>
+        </Link>
       </div>
       <div className="flex space-x-4 items-center">
-        <p className="link">Gmail</p>
-        <p className="link">Images</p>
+        <Link href="https://mail.google.com">
+          <p className="link">Gmail</p>
+        </Link>
+        <p
+          onClick={() =>
+            router.push({
+              pathname: "/search",
+              query: {
+                term: router.query.term || "google",
+                searchType: "image",
+              },
+            })
+          }
+          className="link"
+        >
+          Images
+        </p>
         <User />
       </div>
     </header>
