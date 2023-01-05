d1572278406651512448c1e1c1d1fa588c6fc7b7
Jihoo <53351097+wildcatco@users.noreply.github.com>
2023-01-05 14:43:13 +0900

    Get data from google search api and creat mocked data

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,13 @@
+import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import SearchHeader from "../components/search-header";
+import mockedResponse from "../data/mock-response";
 
-const SearchPage = () => {
+interface Props {
+  searchResults: any;
+}
+
+const SearchPage: NextPage<Props> = ({ searchResults }) => {
   return (
     <div>
       <Head>
@@ -17,3 +23,30 @@ const SearchPage = () => {
 };
 
 export default SearchPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const mockData = true; // ! Should set false after dev
+
+  let data;
+  if (mockData) {
+    data = mockedResponse;
+  } else {
+    const { term, searchType } = context.query;
+    const url = `https://www.googleapis.com/customsearch/v1?key=${
+      process.env.SEARCH_API_KEY
+    }&cx=${process.env.SEARCH_CONTEXT_KEY}&q=${term}${
+      searchType === "image" ? "&searchType=image" : ""
+    }`;
+
+    const response = await fetch(url);
+    data = await response.json();
+  }
+
+  return {
+    props: {
+      searchResults: data,
+    },
+  };
+};
